9eca31012e2d5811552daae1200385ce355edcd5
Alexandra Vesloguzova <sashickk@gmail.com>
2015-12-14 12:05:21 +0300

    Fix incorrect hours calculation

--- app/models/Deadline.scala
+++ app/models/Deadline.scala
@@ -1,12 +1,15 @@
 package models
 
-import org.joda.time.DateTime
+import org.joda.time.{Period, DateTime}
 
 
 /**
   * Created by av on 13/12/15.
   */
-case class Deadline(id: Option[Long], title: String, description: Option[String], date: DateTime)
+case class Deadline(id: Option[Long], title: String, description: Option[String], date: DateTime) {
+  def getLastHours(): Int = new Period(DateTime.now(), date).getHours
+
+}
 
 object Deadline {
   implicit def dateToString(dateTime: DateTime):String = dateTime.toString()
